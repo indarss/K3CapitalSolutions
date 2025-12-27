8bafa415ab98464c280cf7a369e5efea7ab0752b
indarss <82354913+indarss@users.noreply.github.com>
2025-12-27 14:20:49 -0700

    scroll fix
    scroll fix

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -79,7 +79,7 @@ export default function Nav() {
       if (!el) return;
       const navHeight =
         document.querySelector(".nav-wrapper")?.offsetHeight || 78;
-      const extraPadding = id === "services" ? -150 : -50;
+      const extraPadding = id === "services" ? -100 : -50;
       window.scrollTo({
         top: el.offsetTop - navHeight - extraPadding,
         behavior: "smooth",
@@ -94,7 +94,7 @@ export default function Nav() {
         if (el) {
           const navHeight =
             document.querySelector(".nav-wrapper")?.offsetHeight || 78;
-          const extraPadding = id === "services" ? -150 : -50;
+          const extraPadding = id === "services" ? -100 : -50;
           window.scrollTo({
             top: el.offsetTop - navHeight - extraPadding,
             behavior: "smooth",
