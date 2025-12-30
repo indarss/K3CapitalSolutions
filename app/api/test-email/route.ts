0ff2bee2b4f788051284a0bbb5e7c2268b4bd4fa
indarss <82354913+indarss@users.noreply.github.com>
2025-12-29 23:20:14 -0700

    resend configuration
    resend configuration

---
+++ app/api/test-email/route.ts
@@ -0,0 +1,27 @@
+import { Resend } from "resend";
+import { NextResponse } from "next/server";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function GET() {
+  try {
+    const result = await resend.emails.send({
+      from: "noreply@k3capitalsolutions.com",
+      to: ["kristseiduks3@gmail.com"],
+	  cc: ["sparnins@hotmail.com"],
+      subject: "Resend test from Vercel",
+      html: `
+        <h2>Success 🎉</h2>
+        <p>This email was sent from your Vercel app using Resend.</p>
+      `,
+    });
+
+    return NextResponse.json({ success: true, result });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, error: String(error) },
+      { status: 500 }
+    );
+  }
+}
